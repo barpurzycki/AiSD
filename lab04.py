0a225e1739d0d9e09b1878fc1e5849fc4339dd1f
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-11-13 13:49:24 +0100

    Update lab04.py

--- lab04.py
+++ lab04.py
@@ -1,56 +1,54 @@
-from typing import Any, List
+from typing import Any, List, Callable, Union
+import queue
+
 
 class TreeNode:
     value: Any
     children: List['TreeNode']
 
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-
-    def print(self):
-        if self.left:
-            self.left.print()
-        print( self.data),
-        if self.right:
-            self.right.print()
+    def __init__(self, value: Any):
+        self.value: value
+        self.children: List['TreeNode'] = []
 
     def is_leaf(self) -> bool:
-        if TreeNode == None:
-            return False
-        elif self.right == None and self.left == None:
+        if len(self.children) == 0:
             return True
-        return False
-
-    def add(self, data):
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = TreeNode(data)
-                else:
-                    self.left.add(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = TreeNode(data)
-                else:
-                    self.right.add(data)
         else:
-            self.data = data
+            return False
 
-    #def for_each_deep_first(self, visit:Callable[['TreeNode'], None]) -> None:
-    
-    #def for_each_level_order(self, visit:Callable[['TreeNode'], None]) -> None:
-    
-    #def search(self, value: Any) -> Union['TreeNode', None]:
+    def add(self, child: 'TreeNode') -> None:
+        self.children.append(child)
+
+    def for_each_deep_first(self, visit: Callable[['TreeNode'], None]) -> None:
+        visit(self)
+        for i in self.children:
+            i.for_each_deep_first(visit)
+
+    def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
+        visit(self)
+        que = queue.Queue()
+        for i in self.children:
+            que.put(self.children)
+        while(queue.qsize(que) > 0):
+            visit()
 
 
-root = TreeNode(5)
-root.add(10)
-root.add(15)
-root.add(12)
-root.add(30)
-root.add(40)
-root.add(11)
-root.add(9)
-root.print()
+
+
+lisc1 = TreeNode(1)
+lisc2 = TreeNode(2)
+lisc3 = TreeNode(3)
+lisc4 = TreeNode(4)
+lisc5 = TreeNode(5)
+lisc1.add(lisc2)
+lisc1.add(lisc3)
+lisc2.add(lisc4)
+lisc2.add(lisc5)
+
+print(lisc1.is_leaf())
+print(lisc2.is_leaf())
+print(lisc3.is_leaf())
+print(lisc4.is_leaf())
+print(lisc5.is_leaf())
+
+
