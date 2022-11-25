97a16747adece6df600c74b8afaf908e91cba007
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-11-25 12:30:34 +0100

    Update lab05zad.py

--- lab05zad.py
+++ lab05zad.py
@@ -26,27 +26,21 @@ class BinaryNode:
     def traverse_in_order(self, visit: Callable[[Any], None]):
         if self.left_child is not None:
             self.left_child.traverse_in_order(visit)
-
         visit(self)
-
         if self.right_child is not None:
             self.right_child.traverse_in_order(visit)
 
     def traverse_post_order(self, visit: Callable[[Any], None]):
         if self.left_child is not None:
             self.left_child.traverse_post_order(visit)
-
         if self.right_child is not None:
             self.right_child.traverse_post_order(visit)
-
         visit(self)
 
     def traverse_pre_order(self, visit: Callable[[Any], None]):
         visit(self)
-
         if self.left_child is not None:
             self.left_child.traverse_pre_order(visit)
-
         if self.right_child is not None:
             self.right_child.traverse_pre_order(visit)
 
