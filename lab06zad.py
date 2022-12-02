0c488c0ceda9d96f60ec4268b8698a5e5639326f
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-12-02 12:35:09 +0100

    Create lab06zad.py

---
+++ lab06zad.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+class BinaryNode:
+    value: Any
+    left_child: 'BinaryNode'
+    right_child: 'BinaryNode'
+
+    def __init__(self, value, left_child, right_child):
+        self.value = value
+        self.left_child = left_child
+        self.right_child = right_child
+
+    def min(self):
+        
+
+
+
+class BinarySearchTree:
+    root: BinaryNode
+
+    def __init__(self, root):
+        self.root: 'BinaryNode' = root
+
+    # def insert(self, value: Any) -> None:
+    #
+    # def _insert(self, node: BinaryNode, value: Any) -> BinaryNode:
+    #
+    # def insert_list(self, list_: List[Any]) -> None:
+    #
+    # def contains(self, value: Any) -> bool:
+    #
+    # def remove(self, value: Any) -> None:
+    #
+    # def _remove(self, node: BinaryNode, value: Any) -> BinaryNode:
+    #
+    # def show(self):
